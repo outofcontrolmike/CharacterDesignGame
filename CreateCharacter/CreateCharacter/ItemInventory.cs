c32d782e4c9962d32afcc49e100c4fa470ba0fe2
Michael Wilson <outofcontrolmike@gmail.com>
2019-08-01 18:04:40 -0500

    Added an itemInventory Class - work on creating items and then adding those objects to an array

---
+++ CreateCharacter/CreateCharacter/ItemInventory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateCharacterMain
+{
+    /// <summary>
+    /// Purpose is to store an array for instantiated items
+    /// 
+    /// </summary>
+    class ItemInventory
+    {
+
+        /// <summary>
+        /// Intanstiate three items
+        /// </summary>
+        public static void startingItems()
+        {
+
+        }
+
+        /// <summary>
+        /// Add items from starting items to array
+        /// Be sure to only insert them as objects
+        /// </summary>
+        public static void itemInventoryArray()
+        {
+
+        }
+        
+    }
+}
